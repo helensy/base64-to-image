b471fb64c885cbbe0ea9c9587b69f4febda4401e
Gabriel Rodrigues <jipacoding@gmail.com>
2019-01-25 15:40:15 -0400

    ES6, Init Typescript & TSlint

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,70 @@
-'use strict';
+// Module dependencies.
+import fs from 'fs';
 
-/**
- * Module dependencies.
- * @private
- */
-var fs = require('fs');
+interface OptionsInterface {
+  fileName:string;
+  type:string;
+  debug?:boolean;
+}
 
-/**
- * Module exports.
- * @public
- */
-module.exports = base64ToImage;
-
-/**
- * Change base64Str to image and write image file with 
-   the specified file name to the specified file path.
- * @param {String} base64 string (mandatory)
- * @param {String} file path e.g. /opt/temp/uploads/ (mandatory)
- * @return {Object} optionsObj holds image type, image filename, debug e.g.{'fileName':fileName, 'type':type,'debug':true} (optional)
- * @public
- */
-function base64ToImage(base64Str, path, optionalObj) {
-
-    if (!base64Str || !path) {
-        throw new Error('Missing mandatory arguments base64 string and/or path string');
-    }
-
-    var optionalObj = optionalObj || {};
-    var imageBuffer = decodeBase64Image(base64Str);
-    var imageType = optionalObj.type || imageBuffer.type || 'png';
-    var fileName = optionalObj.fileName || 'img-' + Date.now();
-    var abs;
-    var fileName = '' + fileName;
-
-    if (fileName.indexOf('.') === -1) {
-        imageType = imageType.replace('image/', '');
-        fileName = fileName + '.' + imageType;
-    }
-
-    abs = path + fileName;
-    fs.writeFile(abs, imageBuffer.data, 'base64', function(err) {
-        if (err && optionalObj.debug) {
-            console.log("File image write error", err);
-        }
-
-    });
-    return {
-        'imageType': imageType,
-        'fileName': fileName
-    };
+// Function + return for dinamic date
+const defaultOptions = () => {
+  return {
+    fileName: `img-${Date.now()}`,
+    type: 'png',
+    debug: false,
+  };
 };
 
 /**
- * Decode base64 string to buffer.
- *
- * @param {String} base64Str string
- * @return {Object} Image object with image type and data buffer.
- * @public
- */
-function decodeBase64Image(base64Str) {
-    var matches = base64Str.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-    var image = {};
-    if (!matches || matches.length !== 3) {
-        throw new Error('Invalid base64 string');
+Change base64Str to image and write image file with
+the specified file name to the specified file path.
+*/
+const base64ToImage = (base64Str:string, path:string, optionsObj?:OptionsInterface) => {
+
+  if (!base64Str || !path) {
+    throw new Error('Missing mandatory arguments base64 string and/or path string');
+  }
+
+  const imageBuffer = decodeBase64Image(base64Str);
+
+  const options = { ...optionsObj, ...defaultOptions() };
+
+  const fileName = options.fileName;
+  const imageType = options.type || imageBuffer.type;
+
+  let finalFileName = fileName.toString();
+  let finalImageType = imageType;
+
+  if (finalFileName.indexOf('.') === -1) {
+    finalImageType = imageType.replace('image/', '');
+    finalFileName = `${finalFileName}.${finalImageType}`;
+  }
+
+  const abs = path + fileName;
+  fs.writeFile(abs, imageBuffer.data, 'base64', (err:any) => {
+    if (err && options.debug) {
+      console.log('File image write error', err);
     }
+  });
 
-    image.type = matches[1];
-    image.data = new Buffer(matches[2], 'base64');
+  return {
+    fileName: finalFileName,
+    imageType: finalImageType,
+  };
+};
 
-    return image;
-}
+const decodeBase64Image = (base64Str:string) => {
+  const matches = base64Str.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+
+  if (!matches || matches.length !== 3) {
+    throw new Error('Invalid base64 string');
+  }
+
+  return {
+    type: matches[1],
+    data: Buffer.from(matches[2], 'base64'),
+  };
+};
+
+export default base64ToImage;
